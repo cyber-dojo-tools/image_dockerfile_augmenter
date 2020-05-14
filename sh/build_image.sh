f8749a24f31ce5edb56698ba7b5ae3b791959902
Jon Jagger <jon@jaggersoft.com>
2020-05-14 16:10:18 +0100

    Refactoring; update bash scripts

--- sh/build_image.sh
+++ sh/build_image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -Eeu
 
-readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
+readonly MY_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 source "${MY_DIR}/image_name.sh"
 
 docker build --tag $(image_name) "${MY_DIR}/.."
--- sh/test_image.sh
+++ sh/test_image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -Eeu
 
-readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
+readonly MY_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 # don't create TMP_DIR off /tmp because on Docker Toolbox
 # /tmp will not be available on the default VM
 readonly TMP=$(cd ${MY_DIR} && mktemp -d XXXXXX)
