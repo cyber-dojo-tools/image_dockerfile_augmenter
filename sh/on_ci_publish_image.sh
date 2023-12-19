a52116d2731a149fcbed363486ce743bd8aa2814
Faye <faye@kosli.com>
2023-12-19 10:55:34 +0100

    Update variable names for Docker login

--- sh/on_ci_publish_image.sh
+++ sh/on_ci_publish_image.sh
@@ -12,7 +12,7 @@ on_ci_publish_tagged_images()
   fi
   echo 'on CI so publishing image'
   # DOCKER_USERNAME, DOCKER_PASSWORD are in ci context
-  echo "${DOCKER_PASSWORD}" | docker login --username "${DOCKER_USERNAME}" --password-stdin
+  echo "${DOCKER_FOUNDATION_PASS}" | docker login --username "${DOCKER_FOUNDATION_USER}" --password-stdin
   docker push "$(image_name)"
   docker logout
 }
