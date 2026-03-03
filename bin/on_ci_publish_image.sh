4e93d6e01169771b6f5bd592ef7c688ed9a72579
JonJagger <jon@kosli.com>
2026-03-03 08:54:03 +0000

    Push amd64 and arm64 images to registry

--- bin/on_ci_publish_image.sh
+++ bin/on_ci_publish_image.sh
@@ -10,10 +10,24 @@ on_ci_publish_tagged_images()
   if ! on_ci; then
     echo 'not on CI so not publishing image'
     return
+  else
+    echo 'on CI so publishing image'
   fi
-  echo 'on CI so publishing image'
+
+  echo "Pushing $(image_name) to Container Registry"
+
+  # PACKAGES_TOKEN and PACKAGES_USERNAME must be set in the Github Actions workflow
   echo "${PACKAGES_TOKEN}" | docker login ghcr.io -u "${PACKAGES_USERNAME}" --password-stdin
-  docker push "$(image_name)"
+
+  docker buildx build \
+   --push \
+   --provenance=false \
+   --platform linux/amd64,linux/arm64 \
+   --tag $(image_name):latest \
+    "${MY_DIR}/.."
+
+  echo "Successfully pushed $(image_name) to Container Registry"
+
   docker logout
 }
 
