31a3890e8119fd6c06cf7a68c79816a71b4866ef
JonJagger <jon@kosli.com>
2026-03-03 08:41:51 +0000

    Build amd64 and arm64 images

--- bin/build_image.sh
+++ bin/build_image.sh
@@ -4,4 +4,25 @@ set -Eeu
 readonly MY_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 source "${MY_DIR}/image_name.sh"
 
-docker build --tag $(image_name) "${MY_DIR}/.."
+exists=$(docker buildx ls | grep container-builder | wc -l)
+
+if [ $exists -eq 0 ]; then
+docker buildx create \
+    --name container-builder \
+    --driver docker-container \
+    --bootstrap \
+    --use
+fi
+
+docker build \
+  --builder container-builder \
+  --platform linux/amd64,linux/arm64 \
+  --tag $(image_name) \
+  "${MY_DIR}/.."
+
+docker build \
+  --builder container-builder \
+  --load \
+  --platform linux/amd64 \
+  --tag "$(image_name)" \
+  "${MY_DIR}/.."
